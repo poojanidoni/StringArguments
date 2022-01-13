ee1be438200d8c540d6f68eb128192c373107063
poojanidoni <poojanidonixworksz@gmail.com>
2022-01-13 09:04:33 +0530

    classmethod

---
+++ Details.java
@@ -0,0 +1,17 @@
+class Details
+{
+	String name;
+	int id;
+	long number;
+	String doB;
+	String email;
+	
+	Details(String nameOf,int idof,long numberOf,String doBOf,String emailId)
+	{
+		id=idof;
+		name=nameOf;
+		number=numberOf;
+		doB=doBOf;
+		email=emailId;
+	}
+}
