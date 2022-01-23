3ea3aec5eb5dd8fcaf72886cd85a4fb3081d6ab9
poojanidoni <poojanidonixworksz@gmail.com>
2022-01-23 21:34:17 +0530

    StringArguments

---
+++ StringArguments.java
@@ -0,0 +1,119 @@
+class StringArguments
+{
+	//wraper class
+	public static void main(String []args)
+	{
+	String number ="80";
+	int num=Integer.parseInt(number);
+	System.out.println("string to int"+"\t"+num);
+	
+	String date="3";
+	byte dat=Byte.parseByte(date);
+	System.out.println("string to byte"+"\t"+dat);
+	
+	String value ="23";
+	short val=Short.parseShort(value);
+	System.out.println("string to short"+"\t"+val);
+	
+	String values="87";
+	long vals=Long.parseLong(values);
+	System.out.println("String to long"+"\t"+vals);
+	
+	String value1="31";
+	float valu=Float.parseFloat(value1);
+	System.out.println("String to float"+"\t"+valu);
+	
+	//String numb="h";
+	//char no=Character.parseCharacter(numb);
+	//System.out.println("String to char"+"\t"+no);
+	
+	String numb="234";
+	double no=Double.parseDouble(numb);
+	System.out.println("String to double"+no);
+	
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
