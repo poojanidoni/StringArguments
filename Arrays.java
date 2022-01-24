bb55b19efc10322af01838dcf3b3fe455c45369e
poojanidoni <poojanidonixworksz@gmail.com>
2022-01-24 20:47:42 +0530

    wrapper class

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+class Arrays
+{
+	public static void main(String[] args)
+	{
+		int ids[]=new int[6];
+		System.out.println(ids.length);
+		ids=new int[9];
+		System.out.println(ids.length);
+	}
+}
