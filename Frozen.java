54359ac08f61e4430e2885a54890a3ec9564d856
poojanidoni <poojanidonixworksz@gmail.com>
2022-01-27 10:51:42 +0530

    Testing

---
+++ Frozen.java
@@ -0,0 +1,24 @@
+class Frozen
+{
+	int b;
+	Frozen()
+	{
+		this();
+	}
+	
+	Frozen(int b)
+	{
+		this();
+		this.b=b;
+	}
+	public static void main(String []args)
+	{
+		Frozen add=new Frozen();
+	}
+	
+}
+
+
+
+
+
