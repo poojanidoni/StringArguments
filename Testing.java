6d7b4ce73844664a508218984cfee9c9a4f411ca
poojanidoni <poojanidonixworksz@gmail.com>
2022-01-27 10:50:16 +0530

    testing

---
+++ Testing.java
@@ -0,0 +1,16 @@
+class Testing
+{
+	public static void main(String []args)
+	{
+		float num=56.4f;
+		float num11=45.5F;
+		double num22=50.7d;
+		double num33=65.7D;
+		double num44=70.1f;
+		double num55=75.3;
+		long num66=8067365627l;
+		long num77=654321872L;
+		
+		System.out.println(num+"\t"+num11+"\t"+num22+"\t"+num33+"\t"+num44+"\t"+num55+"\t"+num66+"\t"+num77);
+	}
+}
